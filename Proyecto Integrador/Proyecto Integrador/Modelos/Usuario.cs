2b95dd1ad6345179e7963825922f8bacbdf0493a
Daniel Avila <diazavilaangel.ad@gmail.com>
2026-05-12 19:42:30 -0500

    Carpetas

---
+++ Proyecto Integrador/Proyecto Integrador/Modelos/Usuario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Integrador.Modelos
+{
+    internal class Usuario
+    {
+    }
+}
--- Proyecto Integrador/Proyecto Integrador/obj/Debug/net10.0-windows/Proyecto Integrador.AssemblyInfo.cs
+++ Proyecto Integrador/Proyecto Integrador/obj/Debug/net10.0-windows/Proyecto Integrador.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto Integrador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6201fa7ba0cb54c2cbf8fcb405023dcd5a8579e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto Integrador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto Integrador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
