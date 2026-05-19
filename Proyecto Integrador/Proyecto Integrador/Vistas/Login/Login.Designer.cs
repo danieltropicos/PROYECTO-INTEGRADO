ce870e762e539dad44e4cd88e8d5581b5cd36e81
IsaacCu <isaaccujilema2006@gmail.com>
2026-05-18 23:35:28 -0500

    Login Sin terminar

---
+++ Proyecto Integrador/Proyecto Integrador/Vistas/Login/Login.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Proyecto_Integrador.Vistas.Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            labelUsuario = new Label();
+            labelContraseña = new Label();
+            txtUsuario = new TextBox();
+            txtContraseña = new TextBox();
+            btnIngresar = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(btnIngresar);
+            panel1.Controls.Add(txtContraseña);
+            panel1.Controls.Add(txtUsuario);
+            panel1.Controls.Add(labelContraseña);
+            panel1.Controls.Add(labelUsuario);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(161, 52);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(589, 367);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(159, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(265, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Biemvenido, Inicia sesion para acceder";
+            // 
+            // labelUsuario
+            // 
+            labelUsuario.AutoSize = true;
+            labelUsuario.Location = new Point(102, 144);
+            labelUsuario.Name = "labelUsuario";
+            labelUsuario.Size = new Size(62, 20);
+            labelUsuario.TabIndex = 1;
+            labelUsuario.Text = "Usuario:";
+            // 
+            // labelContraseña
+            // 
+            labelContraseña.AutoSize = true;
+            labelContraseña.Location = new Point(102, 185);
+            labelContraseña.Name = "labelContraseña";
+            labelContraseña.Size = new Size(86, 20);
+            labelContraseña.TabIndex = 2;
+            labelContraseña.Text = "Contraseña:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(194, 141);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(276, 27);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(194, 182);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(276, 27);
+            txtContraseña.TabIndex = 4;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(231, 250);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(123, 36);
+            btnIngresar.TabIndex = 5;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.Click += btnIngresar_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(933, 510);
+            Controls.Add(panel1);
+            Name = "Login";
+            Text = "Login";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Button btnIngresar;
+        private TextBox txtContraseña;
+        private TextBox txtUsuario;
+        private Label labelContraseña;
+        private Label labelUsuario;
+    }
+}
---
+++ Proyecto Integrador/Proyecto Integrador/Vistas/Login/Login.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic;
+using Proyecto_Integrador.Controladores;
+using Proyecto_Integrador.Modelos.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Proyecto_Integrador.Vistas.Login
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private string HashContrasena(string contrasena)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(contrasena);
+                byte[] hash = sha256.ComputeHash(bytes);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            
+            UsuarioControlador usuarioControlador = new UsuarioControlador();
+            foreach (var usuario in usuarioControlador.ObtenerUsuarios())
+            {
+                if (usuario.NombreUsuario == txtUsuario.Text && usuario.ContrasenaEncriptada == HashContrasena(txtContraseña.Text))
+                {
+                    MessageBox.Show("Bienvenido.");
+
+                }
+            }
+
+
+        }
+    }
+}
